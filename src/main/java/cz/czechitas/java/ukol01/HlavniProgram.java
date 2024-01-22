67e41c85b4c896b1176948a1e406b76c5c78cfee
Filip Jirsák <filip@jirsak.org>
2024-01-22 23:50:02 +0100

    Aktualizace jaro 2024 – Java 21, Gradle 8.5

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -1,17 +1,17 @@
 package cz.czechitas.java.ukol01;
 
-import cz.czechitas.java.ukol01.engine.*;
+import cz.czechitas.java.ukol01.engine.Turtle;
 
 public class HlavniProgram {
 
-    public void start() {
-        Turtle zofka = new Turtle();
+  public static void main(String[] args) {
+    new HlavniProgram().start();
+  }
 
-        //TODO implementace domácího úkolu
-    }
+  public void start() {
+    Turtle zofka = new Turtle();
 
-    public static void main(String[] args) {
-        new HlavniProgram().start();
-    }
+    //TODO implementace domácího úkolu
+  }
 
 }
