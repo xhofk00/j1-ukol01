2e0cfe9cd38acb05addcbc79c22251f6a190cbd1
Filip Jirsák <filip@jirsak.org>
2024-01-23 19:59:54 +0100

    Turtle zofka jako instanční proměnná.

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -3,13 +3,14 @@ package cz.czechitas.java.ukol01;
 import cz.czechitas.java.ukol01.engine.Turtle;
 
 public class HlavniProgram {
+  private Turtle zofka;
 
   public static void main(String[] args) {
     new HlavniProgram().start();
   }
 
   public void start() {
-    Turtle zofka = new Turtle();
+    zofka = new Turtle();
 
     //TODO implementace domácího úkolu
   }
